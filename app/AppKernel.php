1c06d336c9899d3ea43ac4a32289b24c91899435
mkim <mkim@hq.armd.ru>
2012-02-21 20:02:54 +0400

    New ExtJS tree bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+            new Armd\Bundle\ExtJSBundle\ArmdExtJSBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
