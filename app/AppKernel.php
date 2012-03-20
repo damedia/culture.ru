60c7194708abb3c53e2e6a1abad62e5af120034d
Ilja Suhinin <isuhinin@armd.ru>
2012-03-20 10:29:54 +0300

    добавил бандл темой

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,6 +36,7 @@ class AppKernel extends Kernel
             new Armd\Bundle\ExtJSBundle\ArmdExtJSBundle(),
             new SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle(),
             new Armd\Bundle\AdminBundle\ArmdAdminBundle(),
+            new Armd\Bundle\CmsTemplateBundle\ArmdCmsTemplateBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
