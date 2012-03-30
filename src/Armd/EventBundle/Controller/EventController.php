31c6e026ba230079b47b5e31a6a76130b13c3503
okostyaev <okostyaev@armd.ru>
2012-03-30 20:15:25 +0400

    Cleanup

--- src/Armd/EventBundle/Controller/EventController.php
+++ src/Armd/EventBundle/Controller/EventController.php
@@ -2,11 +2,7 @@
 
 namespace Armd\EventBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Armd\Bundle\NewsBundle\Controller\NewsController as BaseController;
-use Armd\Bundle\CmsBundle\Controller\Controller;
-use Armd\Bundle\CmsBundle\UsageType\UsageType;
 
 class EventController extends BaseController
 {
--- src/Armd/ExtendedNewsBundle/Controller/NewsController.php
+++ src/Armd/ExtendedNewsBundle/Controller/NewsController.php
@@ -2,8 +2,6 @@
 
 namespace Armd\ExtendedNewsBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Armd\Bundle\NewsBundle\Controller\NewsController as BaseController;
 
 class NewsController extends BaseController
