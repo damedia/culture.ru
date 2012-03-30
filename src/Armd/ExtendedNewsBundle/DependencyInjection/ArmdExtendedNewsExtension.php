5f547f8d05ee67132f671ecd6e8a4c054a234c84
okostyaev <okostyaev@armd.ru>
2012-03-30 16:19:47 +0400

    ExtendedNewsBundle

--- src/Armd/ExtendedNewsBundle/DependencyInjection/ArmdExtendedNewsExtension.php
+++ src/Armd/ExtendedNewsBundle/DependencyInjection/ArmdExtendedNewsExtension.php
@@ -23,7 +23,6 @@ class ArmdExtendedNewsExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
         $loader->load('usagetype.yml');
         $loader->load('controller.yml');
         $loader->load('admin.yml');        
