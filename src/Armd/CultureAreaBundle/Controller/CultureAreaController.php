1ab74e5689efbe99d8e00c781640ed649b9a00a7
okostyaev <okostyaev@armd.ru>
2012-03-30 12:56:44 +0400

    CultureAreaController

---
+++ src/Armd/CultureAreaBundle/Controller/CultureAreaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Armd\CultureAreaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class CultureAreaController extends Controller
+{
+}
