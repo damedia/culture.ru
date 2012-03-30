de67f2277aafefcc6b0ac7454ce6946c114c936a
okostyaev <okostyaev@armd.ru>
2012-03-30 13:26:20 +0400

    CultureAreaController

--- src/Armd/CultureAreaBundle/Controller/CultureAreaController.php
+++ src/Armd/CultureAreaBundle/Controller/CultureAreaController.php
@@ -7,5 +7,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Armd\Bundle\CmsBundle\Controller\Controller;
 
 class CultureAreaController extends Controller
-{
+{           
+    public function listAction(UsageType $params)
+    {   
+        $entities = $this->getEntityRepository()->findAll();        
+
+        return $this->renderCms($params, array( 'entities' => $entities ));
+    }
+
+    public function itemAction(UsageType $params, $id)
+    {
+        $entity = $this->getEntityRepository()->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find entity.');
+        }
+
+        return $this->renderCms($params, array( 'entity' => $entity ));
+    }
 }
