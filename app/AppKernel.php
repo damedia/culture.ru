8a77c37c4d27001c6a323a25fd1f06afb75aea95
ivlaskin <ivlaskin@hq.armd.ru>
2012-04-16 19:17:01 +0400

    add tvigle bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -45,6 +45,7 @@ class AppKernel extends Kernel
             new Armd\CommonBundle\ArmdCommonBundle(),
             new Armd\CultureAreaBundle\ArmdCultureAreaBundle(),
             new Armd\TaxonomyBundle\ArmdTaxonomyBundle(),
+            new Armd\Bundle\TvigleBundle\ArmdTvigleBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
