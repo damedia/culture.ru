b17fada4e9ed2f8acf300d10db2c9350d635c594
Oleg Kostyaev <okostyaev@gmail.com>
2012-08-21 16:21:49 +0400

    ArmdUserBundle

---
+++ src/Armd/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Armd\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // add your custom field
+        $builder
+            ->add('lastname')        
+            ->add('firstname')
+            ->add('middlename');
+            
+        parent::buildForm($builder, $options);
+
+        $builder            
+            ->add('sex')
+            ->add('city');
+    }
+
+    public function getName()
+    {
+        return 'armd_user_registration';
+    }
+}
