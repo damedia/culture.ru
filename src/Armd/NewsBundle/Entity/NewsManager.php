86db18fcfc69b85db8bdf798c765720774bc87ab
Oleg Kostyaev <okostyaev@gmail.com>
2012-09-14 19:17:11 +0400

    ArmdMainBundle

--- src/Armd/NewsBundle/Entity/NewsManager.php
+++ src/Armd/NewsBundle/Entity/NewsManager.php
@@ -29,22 +29,13 @@ class NewsManager
         $this->class = $class;
     }
 
-    /**    
-     * @param array $criteria
-     * @param integer $page
-     * @param integer $maxPerPage
-     *
-     * @return \Sonata\AdminBundle\Datagrid\Pager
-     */
-    public function getPager(array $criteria, $page, $maxPerPage = 10)
+    public function getPager(array $criteria, $page, $limit = 10)
     {
-        $pager = new Pager();
-        $pager->setMaxPerPage($maxPerPage);
-        $pager->setQuery(new ProxyQuery($this->getQueryBuilder($criteria)));
-        $pager->setPage($page);
-        $pager->init();
-
-        return $pager;
+        return $this->getQueryBuilder($criteria)
+            ->getQuery()
+            ->setMaxResults($limit)
+            ->getResult()
+        ;
     }
     
     public function getQueryBuilder(array $criteria)
@@ -57,7 +48,7 @@ class NewsManager
             ->innerJoin('n.category', 'c', 'WITH', 'c.slug = :slug')
             ->leftJoin('n.image', 'i', 'WITH', 'i.enabled = true')
             ->andWhere('n.published = true')
-//            ->orderBy('n.date', 'DESC')
+            ->orderBy('n.date', 'DESC')
         ;
 
         $parameters['slug'] = $criteria['category'];
