969c33a5f674bd06feda122205b65805cdc01f32
Oleg Kostyaev <okostyaev@gmail.com>
2012-09-14 13:07:14 +0400

    ArmdMainBundle

--- src/Armd/MainBundle/Controller/BorodinoController.php
+++ src/Armd/MainBundle/Controller/BorodinoController.php
@@ -8,14 +8,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class BorodinoController extends Controller
 {
     function indexAction()
-    {
-        return $this->render("ArmdMainBundle:Borodino:index.html.twig");
+    {    
+        return $this->render('ArmdMainBundle:Borodino:index.html.twig', array(
+        ));
     }
     
+    function eventsAction()
+    {
+        return $this->render('ArmdMainBundle:Borodino:events.html.twig', array(
+            'events'    =>  $this->getEventManager()->getList(),
+        ));
+    }
+        
     function borodinoNewsAction($category)
     {
         $news = $this->get('armd_news.controller.news')->getLatestNewsList(4, 1, array($c->getSlug()));    
         
         return $this->render("ArmdMainBundle:Borodino:news.html.twig");
     }
+    
+    function getEventManager()
+    {
+        return $this->get('armd_event.manager.event');
+    }
+
+    function getNewsManager()
+    {
+        return $this->get('armd_news.manager.news');
+    }    
 }
