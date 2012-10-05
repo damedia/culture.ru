12d95a22fc148257385c36e27213f1a03cadff79
Shamsudin Peyzulaev <info@shams.su>
2012-10-05 17:48:56 +0400

    Export crlf fix1

--- src/Armd/AtlasBundle/Controller/AdminController.php
+++ src/Armd/AtlasBundle/Controller/AdminController.php
@@ -144,6 +144,8 @@ class AdminController extends Controller
                 $filePath = $exportDir . '/atlas_object_' . $object->getId() . '.txt';
                 $content = $this->renderView('ArmdAtlasBundle:Admin:export_objects.txt.twig', array('object' => $object));
                 $content = html_entity_decode($content);
+                $content  = str_replace("\r\n", "\n", $content);
+                $content  = str_replace("\r", "\n", $content);
                 $content  = str_replace("\n", "\r\n", $content);
 
                 echo $filePath . "<br>";
