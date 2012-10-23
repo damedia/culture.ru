afc5c4c5f97ddabed21dcdd880cb14cec894c335
Shamsudin Peyzulaev <info@shams.su>
2012-10-23 11:58:04 +0400

    [*] English banners

--- web/app_ms_en.php
+++ web/app_ms_en.php
@@ -19,7 +19,7 @@ $loader->register(true);
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('ms_en', false);
+$kernel = new AppKernel('ms_en', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
