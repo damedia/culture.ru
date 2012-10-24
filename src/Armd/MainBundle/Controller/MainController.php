c73c1b73d17adeb99d5705f7a8c3d328f16c4712
Shamsudin Peyzulaev <info@shams.su>
2012-10-24 12:05:12 +0400

    [*] Main page ru: show  last event instead of news

--- src/Armd/MainBundle/Controller/MainController.php
+++ src/Armd/MainBundle/Controller/MainController.php
@@ -13,117 +13,151 @@ class MainController extends Controller
     {
         return $this->renderTemplate('banner');
     }
-    
+
     public function museumsAction($page = 1, $limit = 100)
     {
-        return $this->renderTemplate('museums', array(
-            'museums'   => $this->getMuseumManager()->getPager(array(), $page, $limit),
-        ));
+        return $this->renderTemplate(
+            'museums',
+            array(
+                'museums' => $this->getMuseumManager()->getPager(array(), $page, $limit),
+            )
+        );
     }
-    
+
     public function aboutAction()
     {
         return $this->renderTemplate('about');
     }
-    
+
     public function servicesAction()
     {
         return $this->renderTemplate('services');
     }
-	
+
     public function indexAction()
     {
         $categories = $this->getNewsManager()->getCategories();
 
-        return $this->render('ArmdMainBundle::index.html.twig', array(
-            'news'          => $this->getNews($categories),        
-            'categories'    => $categories,
-            'russiaImages'  => $this->getDoctrine()->getRepository('ArmdAtlasBundle:Object')->findRussiaImages(),
-        ));
+        return $this->render(
+            'ArmdMainBundle::index.html.twig',
+            array(
+                'news' => $this->getNews($categories),
+                'events' => $this->getEvents(4),
+                'categories' => $categories,
+                'russiaImages' => $this->getDoctrine()->getRepository('ArmdAtlasBundle:Object')->findRussiaImages(),
+            )
+        );
     }
 
     public function randomRussiaImagesAction()
     {
-        return $this->render('ArmdMainBundle:Main:randomRussiaImages.html.twig', array(
-            'russiaImages' => $this->getDoctrine()->getRepository('ArmdAtlasBundle:Object')->findRandomRussiaImages(6)
-        ));
+        return $this->render(
+            'ArmdMainBundle:Main:randomRussiaImages.html.twig',
+            array(
+                'russiaImages' => $this->getDoctrine()->getRepository('ArmdAtlasBundle:Object')->findRandomRussiaImages(
+                    6
+                )
+            )
+        );
     }
-    
+
     public function latestTopicsAction($domain)
     {
         $topics = array(
             '/export/?module=m_ep_propostal',
-            '/export/?module=m_ep_forum',            
+            '/export/?module=m_ep_forum',
         );
-        
+
         $result = array();
-        foreach ($topics as $url)
-        {
+        foreach ($topics as $url) {
             $content = file_get_contents("http://{$domain}{$url}");
             if ($content !== false) {
                 $obj = json_decode($content);
                 $result[] = $obj->{'data'};
             }
         }
-        
-        return $this->render('ArmdMainBundle:Communication:index.html.twig', array(
-            'topics'    => $result,
-            'domain'    => $domain,
-        )); 
+
+        return $this->render(
+            'ArmdMainBundle:Communication:index.html.twig',
+            array(
+                'topics' => $result,
+                'domain' => $domain,
+            )
+        );
     }
-    
+
     public function pressArchiveAction($format = 'gallery_archive', $category = 'archive')
     {
         $criteria = array(
-            'defaultFormat'    => $format,
+            'defaultFormat' => $format,
         );
-    
-        return $this->render('ArmdMainBundle:Archive:index.html.twig', array(
-            'category'  => $category,
-            'gallery'   => $this->getGalleryManager()->findOneBy($criteria)
-        )); 
+
+        return $this->render(
+            'ArmdMainBundle:Archive:index.html.twig',
+            array(
+                'category' => $category,
+                'gallery' => $this->getGalleryManager()->findOneBy($criteria)
+            )
+        );
     }
-    
+
     function getNews(array $categories)
     {
         $result = array();
-        
-        foreach ($categories as $c)
-        {
-            $result[$c->getSlug()] = $this->get('armd_news.controller.news')->getPaginator(array('category' => $c->getSlug()), 1, 4);    
+
+        foreach ($categories as $c) {
+            $result[$c->getSlug()] = $this->get('armd_news.controller.news')->getPaginator(
+                array('category' => $c->getSlug()),
+                1,
+                4
+            );
         }
 
         return $result;
     }
-    
+
+    function getEvents($limit)
+    {
+        $events = $this->getNewsManager()->getPager(
+            array(
+                'category' => 'events',
+                'has_image' => true,
+            ),
+            1,
+            $limit
+        );
+        return $events;
+    }
+
+
     function renderTemplate($template, $parameters = array())
     {
         return $this->render("ArmdMainBundle::{$template}.html.twig", $parameters);
     }
-    
+
     function getNewsManager()
     {
         return $this->get('armd_news.manager.news');
-    }        
-    
+    }
+
     function getMuseumManager()
     {
         return $this->get('armd_museum.manager.museum');
-    }        
-    
+    }
+
     function getGalleryManager()
     {
         return $this->get('sonata.media.manager.gallery');
-    }        
-    
+    }
+
     function getTemplateName($action)
     {
         return "{$this->getControllerName()}:{$action}.html.twig";
-        
-    }    
-    
+
+    }
+
     function getControllerName()
     {
         return 'ArmdMainBundle:Main';
-    }        
+    }
 }
