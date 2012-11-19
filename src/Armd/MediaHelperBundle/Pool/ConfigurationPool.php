3c78a935e0dc7236ba73cbd73567c37152f2f02f
Shamsudin Peyzulaev <info@shams.su>
2012-11-19 14:17:05 +0400

    [+] culturetv config

---
+++ src/Armd/MediaHelperBundle/Pool/ConfigurationPool.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Armd\MediaHelperBundle\Pool;
+
+#use Symfony\Component\DependencyInjection\ContainerInterface;
+#use Symfony\Component\DependencyInjection\Reference;
+
+
+class ConfigurationPool
+{
+
+    protected $options = array();
+
+    /**
+     * @param array $options
+     * @return void
+     */
+    public function setOptions(array $options = array())
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     * @return void
+     */
+    public function setOption($name, $value)
+    {
+        $this->options[$name] = $value;
+    }
+
+    /**
+     * @param $name
+     * @param null $default
+     * @return null
+     */
+    public function getOption($name, $default = null)
+    {
+        return isset($this->options[$name]) ? $this->options[$name] : $default;
+    }
+}
