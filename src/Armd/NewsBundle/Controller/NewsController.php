876bac5e9cc46697b21670ee31514d04ad5a097f
Shamsudin Peyzulaev <info@shams.su>
2012-12-25 16:53:49 +0400

    [*] move web/js/vendors to web/vendors

--- src/Armd/NewsBundle/Controller/NewsController.php
+++ src/Armd/NewsBundle/Controller/NewsController.php
@@ -106,22 +106,25 @@ class NewsController extends Controller
 
     /**
      * @Route("/", name="armd_news_list_index", options={"expose"=true})
-     * //@Route("/page/{page}/", requirements={"page" = "\d+"}, defaults={"page" = 1}, name="armd_news_list_index_by_page", options={"expose"=true})
      * @Route("/{category}/", requirements={"category" = "[a-z]+"}, name="armd_news_list_index_by_category", options={"expose"=true})
-     * //@Route("/{category}/page/{page}/", requirements={"category" = "[a-z]+", "page" = "\d+"}, defaults={"page" = 1}, name="armd_news_list_index_by_category_and_page", options={"expose"=true})
-     * @Template("ArmdNewsBundle:News:index.html")
+     * @Template("ArmdNewsBundle:News:index.html.twig")
      */
-    function newsIndexAction($category = null, $page = 1, $limit = 10)
+    function newsIndexAction($category = null)
     {
-        return array();
+        return array('category' => $category);
     }
 
 
     /**
-     * @Route("/list"
-     * @param null $category
-     * @param null $startDate
-     * @param null $endDate
+     * @Route("/list-by-date/{category}", name="armd_news_list_by_date")
+     */
+    function newsListByDateAction($category = null)
+    {
+
+    }
+
+    /**
+     * @Route("/list/{category}")
      */
     function newsListAction($category = null)
     {
@@ -134,12 +137,12 @@ class NewsController extends Controller
             );
         }
 
-        $request->get
-
-        return $this->render('ArmdNewsBundle:News:list.html.twig', array(
-            'category'      => $category,
-            'news'          => $this->getNewsManager()-$criteria, $page, $limit),
-        ));
+//        $request->get
+//
+//        return $this->render('ArmdNewsBundle:News:list.html.twig', array(
+//            'category'      => $category,
+//            'news'          => $this->getNewsManager()-$criteria, $page, $limit),
+//        ));
 
     }
     
