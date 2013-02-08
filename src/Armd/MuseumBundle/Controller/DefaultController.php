11f0d8aaf9f5084a3de3d9c7f812db2302a45311
mkim <mkim@hq.armd.ru>
2013-02-08 18:28:05 +0400

    Unstable save

--- src/Armd/MuseumBundle/Controller/DefaultController.php
+++ src/Armd/MuseumBundle/Controller/DefaultController.php
@@ -24,11 +24,54 @@ class DefaultController extends Controller
      */
     public function listAction($page = 1, $perPage = 1000)
     {
+        $museumManager = $this->get('armd_museum.manager.museum');
         $criteria = array(
             'search_string' => $this->getRequest()->get('searchString')
         );
         return array(
-            'museums' => $this->get('armd_museum.manager.museum')->getPager($criteria, $page, $perPage),
+            'museums' => $museumManager->getPager($criteria, $page, $perPage),
         );
+    }
+
+    /**
+     * @Route("/filter", name="armd_museum_filter", defaults={"_format"="json"})
+     */
+    public function filterAction()
+    {
+        try {
+            $filter = $this->getRequest()->get('f');
+
+            $criteria = array(
+                NewsManager::CRITERIA_IS_ON_MAP => true,
+                NewsManager::CRITERIA_HAS_IMAGE => true,
+                NewsManager::CRITERIA_EVENT_DATE_SINCE => empty($filter['date_from']) ? new DateTime : new DateTime($filter['date_from']),
+                NewsManager::CRITERIA_EVENT_DATE_TILL => empty($filter['date_to']) ? new DateTime : new DateTime($filter['date_to']),
+            );
+
+            if (!empty($filter['category'])) {
+                $criteria[NewsManager::CRITERIA_CATEGORY_IDS_OR] = array($filter['category']);
+            } else {
+                throw new \Exception('Выберите хотя бы один тип события.');
+            }
+
+            $museums = $this->getNewsManager()->findObjects($criteria);
+
+            $data = array();
+
+            return array(
+                'success' => true,
+                'message' => 'OK',
+                'result' => array(
+                    'filter' => $filter,
+                    'data' => $data,
+                ),
+            );
+        }
+        catch (\Exception $e) {
+            return array(
+                'success' => false,
+                'message' => $e->getMessage(),
+            );
+        }
     }
 }
--- src/Armd/MuseumBundle/Entity/MuseumManager.php
+++ src/Armd/MuseumBundle/Entity/MuseumManager.php
@@ -3,75 +3,48 @@
 namespace Armd\MuseumBundle\Entity;
 
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\QueryBuilder;
 use Armd\SphinxSearchBundle\Services\Search\SphinxSearch;
 use Knp\Component\Pager\Paginator;
+use Armd\ListBundle\Entity\ListManager;
 
-class MuseumManager
+class MuseumManager extends ListManager
 {
-    /**
-     * @var \Doctrine\ORM\EntityManager
-     */
-    protected $em;
-    protected $paginator;
-    protected $search;
+    /** example: array(13, 7) */
+    const CRITERIA_CATEGORY_IDS_OR = 'CRITERIA_CATEGORY_IDS_OR';
 
-    /**
-     * @param \Doctrine\ORM\EntityManager $em
-     * @param \Knp\Component\Pager\Paginator $paginator
-     * @param \Armd\SphinxSearchBundle\Services\Search\SphinxSearch $search
-     */
-    public function __construct(EntityManager $em, Paginator $paginator, SphinxSearch $search)
+    public function getQueryBuilder()
     {
-        $this->em = $em;
-        $this->paginator = $paginator;
-        $this->search = $search;
-    }
-    
-    public function getPager(array $criteria, $page = 1, $perPage = 10)
-    {
-        if (empty($criteria['search_string'])) {
+        $qb = $this->em->getRepository('ArmdMuseumBundle:Museum')
+            ->createQueryBuilder('_museum');
 
-            $museumQb = $this->getQueryBuilder('m', $criteria)->getQuery();
-            $pagination = $this->paginator->paginate($museumQb, $page, $perPage);
-
-        } else {
-
-            $searchParams = array(
-                'Museums' => array(
-                    'result_offset' => ($page - 1) * $perPage,
-                    'result_limit' => $perPage,
-                    'sort_mode' => '@relevance DESC, @weight DESC'
-                )
-            );
-
-            $searchResult = $this->search->search($criteria['search_string'], $searchParams);
-            $museums = array();
-            if (!empty($searchResult['Museums']['matches'])) {
-                foreach ($searchResult['Museums']['matches'] as $id => $data) {
-                    $museum = $this->em->getRepository('ArmdMuseumBundle:Museum')->find($id);
-                    if (!empty($museum)) {
-                        $museums[] = $museum;
-                    }
-                }
-            }
-            $pagination = $this->paginator->paginate($museums, $page, $perPage);
-            $pagination->setTotalItemCount($searchResult['Museums']['total']);
-
-        }
-
-        return $pagination;
-    }
-    
-    public function getQueryBuilder($rootAlias, $criteria)
-    {
-        $query = $this->em->getRepository('ArmdMuseumBundle:Museum')->createQueryBuilder($rootAlias)
-            ->addSelect($rootAlias . ', _i')
-            ->leftJoin($rootAlias . '.image', '_i', 'WITH', '_i.enabled = true')
-            ->andWhere($rootAlias . '.published = true')
-            ->orderBy($rootAlias . '.title')
+        $qb->innerJoin('_museum.category', '_museumCategory')
+            ->leftJoin('_museum.image', '_museumImage', 'WITH', '_museumImage.enabled = TRUE')
+            ->andWhere('_museum.published = TRUE')
+            ->orderBy('_museum.title', 'DESC')
         ;
-                
-        return $query;
+
+        return $qb;
+    }
+
+    public function setCriteria(QueryBuilder $qb, $criteria)
+    {
+        parent::setCriteria($qb, $criteria);
+
+        if (!empty($criteria[self::CRITERIA_CATEGORY_IDS_OR])) {
+            $qb->andWhere('_museum.category IN (:category_ids_or)')
+                ->setParameter('category_ids_or', $criteria[self::CRITERIA_CATEGORY_IDS_OR]);
+        }
+    }
+
+    public function getClassName()
+    {
+        return 'Armd\MuseumBundle\Entity\Museum';
+    }
+
+    public function getTaggableType()
+    {
+        return 'armd_museum';
     }
 
 }
