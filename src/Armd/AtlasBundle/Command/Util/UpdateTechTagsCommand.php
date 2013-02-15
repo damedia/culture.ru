aac678f1747082bfd7bd73d72d99c03d0d45d46d
nnikolaev <nnikolaev@itrack.ru>
2013-02-15 17:15:32 +0400

    autotags for Objects and Lectures

---
+++ src/Armd/AtlasBundle/Command/Util/UpdateTechTagsCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Armd\AtlasBundle\Command\Util;
+
+use Doctrine\Bundle\DoctrineBundle\Command\DoctrineCommand;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+
+class UpdateTechTagsCommand extends DoctrineCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('armd-mk:atlas:update-tech-tags')
+            ->setDescription('Update tech tags');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Execution...');
+        $em = $this->getEntityManager('default');
+        $tagManager = $this->getContainer()->get('fpn_tag.tag_manager');
+        $entities = $em->getRepository('ArmdAtlasBundle:Object')->findAll();
+        
+        foreach ($entities as $entity) {
+            $tag = $tagManager->loadOrCreateTag('o' . $entity->getId(), false);
+            $tagManager->addTag($tag, $entity);
+            $tagManager->saveTagging($entity, false);              
+        }
+        
+        $em->flush();      
+        
+        $output->writeln('<info>Update tech tags completed</info>');
+    }
+
+
+}
--- src/Armd/TagBundle/Entity/TagManager.php
+++ src/Armd/TagBundle/Entity/TagManager.php
@@ -2,6 +2,7 @@
 namespace Armd\TagBundle\Entity;
 
 use FPN\TagBundle\Entity\TagManager as BaseTagManager;
+use DoctrineExtensions\Taggable\Taggable;
 
 class TagManager extends BaseTagManager
 {
@@ -37,6 +38,101 @@ class TagManager extends BaseTagManager
     public function getTagClass()
     {
         return $this->tagClass;
+    }
+    
+    public function loadOrCreateTag($name, $doFlush = true)
+    {
+        $tags = $this->loadOrCreateTags(array($name), $doFlush);
+        return $tags[0];
+    }
+    
+    public function loadOrCreateTags(array $names, $doFlush = true)
+    {
+        if (empty($names)) {
+            return array();
+        }
+
+        $names = array_unique($names);
+
+        $builder = $this->em->createQueryBuilder();
+
+        $tags = $builder
+            ->select('t')
+            ->from($this->tagClass, 't')
+
+            ->where($builder->expr()->in('t.name', $names))
+
+            ->getQuery()
+            ->getResult()
+        ;
+
+        $loadedNames = array();
+        foreach ($tags as $tag) {
+            $loadedNames[] = $tag->getName();
+        }
+
+        $missingNames = array_udiff($names, $loadedNames, 'strcasecmp');
+        if (sizeof($missingNames)) {
+            foreach ($missingNames as $name) {
+                $tag = $this->createTag($name);
+                $this->em->persist($tag);
+
+                $tags[] = $tag;
+            }
+            
+            if ($doFlush) {
+                $this->em->flush();
+            }
+        }
+
+        return $tags;
+    }
+    
+    public function saveTagging(Taggable $resource, $doFlush = true)
+    {
+        $oldTags = $this->getTagging($resource);
+        $newTags = $resource->getTags();
+        $tagsToAdd = $newTags;
+
+        if ($oldTags !== null and is_array($oldTags) and !empty($oldTags)) {
+            $tagsToRemove = array();
+
+            foreach ($oldTags as $oldTag) {
+                if ($newTags->exists(function ($index, $newTag) use ($oldTag) {
+                    return $newTag->getName() == $oldTag->getName();
+                })) {
+                    $tagsToAdd->removeElement($oldTag);
+                } else {
+                    $tagsToRemove[] = $oldTag->getId();
+                }
+            }
+
+            if (sizeof($tagsToRemove)) {
+                $builder = $this->em->createQueryBuilder();
+                $builder
+                    ->delete($this->taggingClass, 't')
+                    ->where('t.tag_id')
+                    ->where($builder->expr()->in('t.tag', $tagsToRemove))
+                    ->andWhere('t.resourceType = :resourceType')
+                    ->setParameter('resourceType', $resource->getTaggableType())
+                    ->andWhere('t.resourceId = :resourceId')
+                    ->setParameter('resourceId', $resource->getTaggableId())
+                    ->getQuery()
+                    ->getResult()
+                ;
+            }
+        }
+
+        foreach ($tagsToAdd as $tag) {
+            $this->em->persist($tag);
+            $this->em->persist($this->createTagging($tag, $resource));
+        }
+
+        if (count($tagsToAdd)) {
+            if ($doFlush) {
+                $this->em->flush();
+            }
+        }
     }
 
 }
