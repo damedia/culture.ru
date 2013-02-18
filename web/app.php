dbb61d3f666d5883ad0533f276958dbce3f29fa6
Shamsudin Peyzulaev <info@shams.su>
2013-02-18 19:51:54 +0400

    [*] disable sf2 cache

--- web/app.php
+++ web/app.php
@@ -21,7 +21,7 @@ require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+//$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
