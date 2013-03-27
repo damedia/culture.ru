02b9642e0d9464e83689425c3f01a0cdf72d594a
Alexey Shockov <alexey@shockov.com>
2013-03-27 11:18:01 +0400

    [+] ArmdExternalSearchBundle (refs #56822)

---
+++ src/Armd/ExternalSearchBundle/Controller/SearchController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Armd\ExternalSearchBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @author Alexey Shockov <alexey@shockov.com>
+ */
+class SearchController extends Controller
+{
+    /**
+     * @Route("/results", name="armd_external_search_results")
+     * @Template
+     */
+    public function resultListAction()
+    {
+        $yandexSearchId = $this->container->get('armd_external_search.search_engine.yandex')->getSearchId();
+
+        return array(
+            'yandexSearchId' => $yandexSearchId,
+        );
+    }
+}
