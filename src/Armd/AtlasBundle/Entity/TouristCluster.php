227ed2ffe697fccd694aeffd6277584b288aa18b
mborisov.dcl <mborisov.dcl@gmail.com>
2013-04-02 12:30:51 +0400

    [+] Issue #55714. TouristCluster Entity added. Dashboard access TC Entity added.

---
+++ src/Armd/AtlasBundle/Entity/TouristCluster.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Armd\AtlasBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Armd\AtlasBundle\Entity\TouristCluster
+ *
+ * @ORM\Table(name="atlas_tourist_cluster")
+ * @ORM\Entity
+ */
+class TouristCluster
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(name="sort_index", type="integer", nullable=false)
+     */
+    private $sortIndex = 0;
+
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getTitle();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Region
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getSortIndex()
+    {
+        return $this->sortIndex;
+    }
+
+    public function setSortIndex($sortIndex)
+    {
+        $this->sortIndex = $sortIndex;
+    }
+}
