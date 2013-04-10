8d24fa413e97162019adffd4b23bcd8920ac596c
Моисеева Екатерина <emoiseeva@spirit.armd.ru>
2013-04-10 12:57:22 +0400

    Kids. Feedback page

---
+++ web/kids/js/kids_map_new.js
@@ -0,0 +1,85 @@
+
+$(function(){
+			
+    $('#mos').data('clicked', true);
+    
+    $('.point').hover( function(){
+        $(this).addClass('active');
+        
+    }, function(){
+        if( !$(this).data('clicked')) {
+            $(this).removeClass('active');
+        }
+    })
+    
+    
+    
+    $('.map-point').on('click', $(this), function(){
+        var point = $(this).attr('id'),
+            pointName = $('.lenta-wrap', $(this)).html(),
+            forSlider = $('.for-slider', $(this)).html();
+        
+        $(this).siblings().data('clicked', false);
+        
+        if($(this).data('clicked')) {
+            $(this).data('clicked', false);
+            $(this).removeClass('active');	
+            $('#window').find('.lenta-big-wrap').html('').end()
+                        .find('.object-slider-content').html('').end()
+                        .removeClass()
+                        .hide();
+        } else {
+            $(this).data('clicked', true);
+            $(this).addClass('active').siblings('.point').removeClass('active');	
+            $('#window').find('.lenta-big-wrap').html(pointName).end()
+                        .find('.object-slider-content').html(forSlider).end()
+                        .removeClass().addClass(point+'_window')
+                        .show();
+            //window.location = '#window';			
+        }
+        
+        
+        
+        
+    })
+    
+    /*FEEDBACK*/
+    
+    jqOLD("textarea#message").cuText({
+        showArrows: false
+    });
+    
+    $('#submit-btn').on('click', $(this), function(){
+        if ($(this).hasClass('.submit-btn-inactive')) {
+            return false;
+        }    
+    })
+    
+    $('#feedback-popup').on('keyup', $('input, textarea'), function(){
+        var empty = false;
+        $('input[required],textarea[required]').each(function(){
+            if ($(this).val() == '') {
+                empty = true;
+                return;
+            }
+        })
+        
+        if (!empty) {
+            $('#submit-btn').removeClass('submit-btn-inactive');
+        } else {
+            $('#submit-btn').addClass('submit-btn-inactive');
+        }
+        
+    })
+    
+    jqOLD("a.modal").fancybox({
+        padding: 0,
+        autoDimensions: false,
+        width: "auto",
+        height: "auto"
+    });
+    
+    $('.fancy-hide').css('visibility','visible').hide();
+    
+    
+})
