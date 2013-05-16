de55481456919b910da4287cd1896eaab1e9aa88
sglinnik <sglinnik@armd.ru>
2013-05-16 15:28:22 +0400

    [*] #60131 Load full atlas on homepage in one request instead of ajax per point

--- src/Armd/AtlasBundle/Controller/DefaultController.php
+++ src/Armd/AtlasBundle/Controller/DefaultController.php
@@ -336,6 +336,7 @@ class DefaultController extends Controller
             if (!$objects)
                 throw new \Exception('Not found');
 
+            $currentUser = $this->get('security.context')->getToken()->getUser();
             $allCategoriesIds = $repo->fetchObjectsCategories($objects);
 
             $rows = array();
@@ -349,11 +350,16 @@ class DefaultController extends Controller
 
                 $obraz = false;
                 $imageUrl = '';
+                $sideDetails = '';
                 if ($obj->getPrimaryCategory()) {
                     if ($obj->getPrimaryCategory()->getId() == 74) {
                         $obraz = true;
                         $image = $obj->getPrimaryImage(); // @TODO Много запросов
                         $imageUrl = $twigExtension->path($image, 'thumbnail');
+                        $sideDetails = $this->renderView('ArmdAtlasBundle:Default:object_side.html.twig', array(
+                            'entity' => $obj,
+                            'editable' => ($obj->getCreatedBy() == $currentUser),
+                        ));
                     }
                 }
 
@@ -366,6 +372,7 @@ class DefaultController extends Controller
                     'icon' => $iconUrl,
                     'obraz' => $obraz,
                     'imageUrl' => $imageUrl,
+                    'sideDetails' => $sideDetails,
                 );
             }
 
