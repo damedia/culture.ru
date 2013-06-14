3ba4efb8034d0eccc86028e2e7891cc6b86c466d
Shamsudin Peyzulaev <info@shams.su>
2013-06-14 12:39:57 +0400

    [^] #62753 fix migration

--- app/DoctrineMigrations/Version20130614123139.php
+++ app/DoctrineMigrations/Version20130614123139.php
@@ -13,7 +13,8 @@ class Version20130614123139 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("DELETE FROM subscription_issue;");
+        $this->addSql("DELETE FROM subscription_issue");
+        $this->addSql("DELETE FROM user_mailing_list WHERE mailing_list_id IN (SELECT id FROM subscription_mailing_list WHERE type = 'new_content' OR type = 'custom')");
         $this->addSql("DELETE FROM subscription_mailing_list WHERE type = 'new_content' OR type = 'custom'");
     }
 
