f633b33110fc29f43a273984f9ba8d797fb26c56
Valentin <v.karnauhov@axioma.lv>
2013-08-06 15:33:06 +0300

    #11832

---
+++ src/Armd/PressCenterBundle/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Armd\PressCenterBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+
+    public function viewAction()
+    {
+        exit('view blog');
+    }
+}
---
+++ src/Armd/PressCenterBundle/Entity/PressCenterRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Armd\PressCenterBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class PressCenterRepository extends EntityRepository
+{
+
+}
