cb7bed3afaf76865f629107314f3b90b9d9121b5
MathObsessed <eric.lebedev@damedia.org>
2013-08-14 12:04:16 +0400

    - Modal layer is now showing during autocomplete process (just a prototype)

--- src/Damedia/TinymceBundle/Resources/public/vendor/tiny_mce/plugins/snippet/plugin.js
+++ src/Damedia/TinymceBundle/Resources/public/vendor/tiny_mce/plugins/snippet/plugin.js
@@ -8,49 +8,65 @@ tinymce.PluginManager.add("snippet", function(editor, url) {
         var modalWindowDiv = $('#snippetBootstrapModal'),
             entitySelect = $('select[name="entity"]', modalWindowDiv),
             autocompleteInput = $('input[name="object"]', modalWindowDiv),
-            entity,
+            selectedEntity,
+            modalLayerDivId = 'modal-layer',
+            modalLayerImgSrc = editor.settings.parameters.modalLayerImgSrc,
             setEntity = function(){
                 var entityTitle,
                     entityTitleSpan = $('span[name="entityTitle"]', modalWindowDiv);
 
                 entityTitle = entitySelect.find('option:selected').text();
                 entityTitleSpan.text(entityTitle);
-                entity = entitySelect.val();
+                selectedEntity = entitySelect.val();
                 autocompleteInput.val('');
             },
             constructAjaxUrl = function(entity, searchTerm){
                 return editor.settings.parameters.acAjaxUrl + '?_sonata_admin=' + editor.settings.parameters.sonataAdmin + '&entity=' + entity + '&q=' + searchTerm;
             },
+            createModalLayer = function(){
+                var div = $('<div id="' + modalLayerDivId + '"><img src="' + modalLayerImgSrc + '" /></div>'),
+                    width = $(document).width(),
+                    height = $(document).height();
+
+                div.css({'width': width, 'height': height}).appendTo("body", document);
+            },
+            removeModalLayer = function(){
+                $("#"+modalLayerDivId).remove();
+            },
             getSearchResults = function(request, response){
-                $.ajax(constructAjaxUrl(entity, request.term), {
+                $.ajax(constructAjaxUrl(selectedEntity, request.term), {
+                    beforeSend: function(){
+                        createModalLayer();
+                    },
+                    complete: function(){
+                        removeModalLayer();
+                        //create report?
+                    },
                     success: function(data){
-                        if (data.length) {
-                            autocompleteInput.removeClass("ui-state-error");
-                        }
-                        else {
-                            autocompleteInput.addClass("ui-state-error");
-                        }
+                        //if (data.length) {
+                        //    autocompleteInput.removeClass("ui-state-error");
+                        //}
+                        //else {
+                        //    autocompleteInput.addClass("ui-state-error");
+                        //}
 
                         response(data);
-                    },
+                    }
+                    /*
                     error: function(xhr, err){
                         autocompleteInput.addClass("ui-state-error");
                     }
+                    */
                 });
             },
             insertSnippet = function(label, value){
-                var snippet = { entityId: value,
-                                label: label,
-                                type: entity,
-                                viewId: null
-                              },
-                    snippetValue,
+                var snippetLabel,
                     snippetTwig,
                     snippetHtml;
 
-                snippetValue = 'Type: ' + snippet.type + ', ID: ' + snippet.entityId + ', Label: ' + snippet.label;
-                snippetTwig = '{% render url(\'damedia_foreign_entity\', { \'entity\': \'' + snippet.type + '\', \'itemId\': ' + snippet.entityId + ' }) %}';
-                snippetHtml = '<input type="button" class="snippet" value="' + snippetValue + '" data-twig="' + snippetTwig + '" disabled="disabled" /> ';
+                snippetLabel = 'Type: ' + selectedEntity + ', ID: ' + value + ', Label: ' + label;
+                snippetTwig = '{% render url(\'damedia_foreign_entity\', { \'entity\': \'' + selectedEntity + '\', \'itemId\': ' + value + ' }) %}';
+                snippetHtml = '<input type="button" class="snippet" value="' + snippetLabel + '" data-twig="' + snippetTwig + '" disabled="disabled" /> ';
 
                 editor.execCommand('mceInsertContent', false, snippetHtml);
 
@@ -65,7 +81,7 @@ tinymce.PluginManager.add("snippet", function(editor, url) {
             setEntity();
         });
 
-        //using 'unbind()' is inevitable here else 'autocompleteselect' callback will be binded many times!
+        //using 'unbind()' here is inevitable else 'autocompleteselect' callback will be binded many times!
         autocompleteInput.autocomplete({ source: getSearchResults }).unbind('autocompleteselect').on('autocompleteselect', function(event, ui){
             insertSnippet(ui.item.label, ui.item.value);
         });
