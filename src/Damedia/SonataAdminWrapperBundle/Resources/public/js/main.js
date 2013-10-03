a254646a0b7fbd3bcbacefd13e76c1c607b80c6f
MathObsessed <eric.lebedev@damedia.org>
2013-10-03 14:08:24 +0400

    Link button (in the top right corner) to view edited object from Sonata Admin has been implemented. Resolved objects for now: News, Movie ("Cinema").

--- src/Damedia/SonataAdminWrapperBundle/Resources/public/js/main.js
+++ src/Damedia/SonataAdminWrapperBundle/Resources/public/js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-    var gotoButton = $('<div class="goto-button"><a href="#">Go to page</a></div>'),
-        gotoButtonPlaceholder = $('.sonata-bc .navbar-text', document);
+    var gotoButton = $('<div class="goto-button"><a target="_blank" href="#">Перейти на страницу</a></div>'),
+        gotoButtonPlaceholder = $('.sonata-bc .navbar-text', document),
+        gotoButtonUrl = AdminEntitiesUrlMapper.resolveCurrentUrl();
 
-    gotoButtonPlaceholder.prepend(gotoButton);
+    if (gotoButtonUrl) {
+        gotoButton.find('a').attr('href', gotoButtonUrl);
+        gotoButtonPlaceholder.prepend(gotoButton);
+    }
 });
